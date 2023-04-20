22f5813fd898f02d044a2f03ee770a73c2e94cc9
rachel-brod <124601086+rachel-brod@users.noreply.github.com>
2023-04-19 21:01:44 -0700

    Add files via upload

---
+++ js/modules/init.js
@@ -0,0 +1,12 @@
+//use async / await to load JSON data
+async function fetchEmployees() {
+    try {
+        const response = await fetch('/data/employees.json')
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export {fetchEmployees}
